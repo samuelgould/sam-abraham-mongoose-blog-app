77032df5c4fd793ffdd6a87cd639a82863ba10de
Sam Gould <sjg326@gmail.com>
2018-01-05 14:06:44 -0800

    update sql file to create tag and stories_tag tables and populate them in preparation for version 3

--- db/query.sql
+++ db/query.sql
@@ -1,5 +1,6 @@
+DROP TABLE IF EXISTS stories_tags;
+DROP TABLE IF EXISTS tags;
 DROP TABLE IF EXISTS stories;
-
 DROP TABLE IF EXISTS authors;
 
 CREATE TABLE authors(
@@ -14,6 +15,19 @@ INSERT INTO authors
     ('georgia', 'georgia@ggg.com'),
     ('gardy', 'gardy@ggg.com'),
     ('tiff', 'tiff@ggg.com');
+
+CREATE TABLE tags(
+    id serial PRIMARY KEY,
+    tag text NOT NULL
+);
+
+INSERT INTO tags
+    (tag) VALUES
+    ('Lady Gaga'),
+    ('Cats'),
+    ('Happiness'),
+    ('Investment'),
+    ('Swag');
 
 CREATE TABLE stories(
     id serial PRIMARY KEY,
@@ -36,3 +50,16 @@ INSERT INTO stories
     (2, '11 ways investing in cats can make you a millionaire','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
     (3, 'Why you should forget everything you learned about cats','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
 
+CREATE TABLE stories_tags(
+    stories_ref int REFERENCES stories ON DELETE CASCADE,
+    tag_ref int REFERENCES tags ON DELETE RESTRICT
+);
+
+INSERT INTO stories_tags
+    (stories_ref, tag_ref) VALUES
+    (1000, 4),
+    (1001, 2),
+    (1002, 1), (1002, 2),
+    (1004, 4), (1004, 3),
+    (1006, 2), (1006, 3), (1006, 4),
+    (1008, 5), (1008, 2);
