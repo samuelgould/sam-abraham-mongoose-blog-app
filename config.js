37b118325caea8c62abb7b60eff3e51caa72bac4
Sam Gould <sjg326@gmail.com>
2018-01-09 16:49:54 -0800

    updated config to find db on mlab

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 const DATABASE_URL = process.env.DATABASE_URL
 	|| global.DATABASE_URL
-	|| 'mongodb://localhost/blogDb';
+	|| 'mongodb://dev:dev@ds041546.mlab.com:41546/blog-db';
 
 exports.DATABASE_URL = DATABASE_URL;
 
